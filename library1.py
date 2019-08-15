69dfb1cb4eec246d33687a19f9e0904759e9bde1
FusionPower <salas._@hotmail.com>
2019-08-15 15:31:43 -0500

    main and library1

---
+++ library1.py
@@ -0,0 +1,5 @@
+class lib1:
+    def __init__(self,m,n):
+        self.x=m
+        self.y=n
+        self.s=m+n
---
+++ main.py
@@ -0,0 +1,11 @@
+#aquí traemos la libreria que permite sumar
+from library1 import *
+
+
+print("Teclea el primer numero:")
+r=int(input())
+print("Teclea el segundo numero:")
+i=int(input())
+num=lib1(r,i)
+print(num.s)
+print("PUM!!! SUMADOS!!!")
